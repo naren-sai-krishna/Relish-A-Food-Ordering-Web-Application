7854a1a0769aad117f94a763c0abdf0f991d9348
Naren Sai Krishna <39124232+naren-sai-krishna@users.noreply.github.com>
2019-01-08 23:40:05 +0530

    Commit @

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost","root","naren","food-ordering");//All communication between PHP and the MySQL database server takes place through this connection.
-// Check connection
-if (mysqli_connect_errno())//The mysqli_connect_errno() function returns the error code from the last connection error, if any.
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>
